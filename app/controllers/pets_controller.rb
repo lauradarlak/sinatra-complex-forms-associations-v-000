a96cb318a5ef0b6fa2158a4e5ed932af39e1af14
Laura Darlak <laura@polyporedesign.com>
2018-10-29 00:06:52 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,21 +1,21 @@
 # class PetsController < ApplicationController
-# 
+#
 #   get '/pets' do
 #     @pets = Pet.all
 #     erb :'/pets/index'
 #   end
-# 
+#
 #   get '/pets/new' do
 #     @owners = Owner.all
 #     erb :'/pets/new'
 #   end
-# 
+#
 #   get '/pets/:id/edit' do
 #     @pet = Pet.find(params[:id])
 #     @owners = Owner.all
 #     erb :'/owners/edit'
 #   end
-# 
+#
 #   post '/pets' do
 #     @pet = Pet.create(params[:pet])
 #     if !params["owner"]["name"].empty?
@@ -23,24 +23,24 @@
 #     end
 #     redirect to "pets/#{@pet.id}"
 #   end
-# 
+#
 #   get '/pets/:id' do
 #     @pet = Pet.find(params[:id])
 #     erb :'/pets/show'
 #   end
-# 
+#
 #   post '/pets/:id' do
 #     # bug fix
 #     if !params[:owner].keys.include?("pet_ids")
 #       params[:owner][:pet_ids] = []
 #     end
 #     # end bug fix
-# 
+#
 #     @owner.update(params[:owner])
 #     if !params[:pet][:name].empty?
 #       @owner.pets << Pet.create(name: params[:pet][:name])
 #     end
-# 
+#
 #     redirect to "pets/#{@pet.id}"
 #   end
 # end
@@ -49,14 +49,14 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
@@ -65,12 +65,12 @@ class PetsController < ApplicationController
     redirect "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
@@ -80,7 +80,7 @@ class PetsController < ApplicationController
     redirect "pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
